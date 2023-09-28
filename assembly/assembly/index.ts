54dec1fbcdcb3503a215924cbffd10c388414f84
Tucker Whitaker <trw2005.50@gmail.com>
2023-09-28 13:19:33 -0500

    reflections

--- assembly/assembly/index.ts
+++ assembly/assembly/index.ts
@@ -32,13 +32,20 @@ class Vector {
 		let length = Math.sqrt(this.x * this.x + this.y * this.y + this.z * this.z);
 		return new Vector(this.x / length, this.y / length, this.z / length);
 	}
+	reflect(normal: Vector): Vector {
+		return this.sub(normal.multiply(2 * this.dot(normal)));
+	}
 }
 class Ray {
 	constructor(public origin: Vector, public direction: Vector) {}
 }
 
 class Sphere {
-	constructor(public center: Vector, public radius: number) {}
+	constructor(
+		public center: Vector,
+		public radius: number,
+		public albedo: Vector
+	) {}
 
 	intersect(ray: Ray): number {
 		const oc = ray.origin.sub(this.center);
@@ -54,45 +61,134 @@ class Sphere {
 	}
 }
 
-export function render(pixelX: number, pixelY: number): u32 {
-	const sphere = new Sphere(new Vector(0, 0, 0), 0.5);
+class HitInfo {
+	didHit: bool = false;
+	Position: Vector = new Vector(0, 0, 0);
+	Index: number = 0;
+	Normal: Vector = new Vector(0, 0, 0);
+
+	constructor(didHit: bool, Position: Vector, Index: number, Normal: Vector) {
+		this.didHit = didHit;
+		this.Position = Position;
+		this.Index = Index;
+		this.Normal = Normal;
+	}
+}
+function traceRay(
+	rayOrigin: Vector,
+	rayDirection: Vector,
+	spheres: Sphere[]
+): HitInfo {
+	let closest = Infinity;
+	let closestIndex = -1;
+
+	for (let i = 0; i < spheres.length; i++) {
+		let sphere = spheres[i];
+		let t = sphere.intersect(new Ray(rayOrigin, rayDirection));
+
+		if (t != -1 && t < closest && t > 0) {
+			closest = t;
+			closestIndex = i;
+		}
+	}
+
+	if (closestIndex == -1) {
+		return new HitInfo(false, new Vector(0, 0, 0), 0, new Vector(0, 0, 0));
+	} else {
+		let hitPosition = rayOrigin.add(rayDirection.multiply(closest));
+		return new HitInfo(
+			true,
+			hitPosition,
+			closestIndex,
+			hitPosition.sub(spheres[closestIndex].center).normalize()
+		);
+	}
+}
+
+function calculateColor(
+	hitInfo: HitInfo,
+	lightDirection: Vector,
+	spheres: Sphere[],
+	closestIndex: i32
+): Array<u32> {
+	if (!hitInfo.didHit) {
+		return [0, 0, 0];
+	}
+
+	let sphere = spheres[closestIndex];
+	let normal = hitInfo.Position.sub(sphere.center).normalize();
+	let light = Math.max(0, normal.dot(lightDirection));
+
+	let R = u32(Math.min(Math.max(0, sphere.albedo.x * light), 255));
+	let G = u32(Math.min(Math.max(0, sphere.albedo.y * light), 255));
+	let B = u32(Math.min(Math.max(0, sphere.albedo.z * light), 255));
+
+	return [R, G, B];
+	//return 0x00000000 | (R << 16) | (G << 8) | B;
+}
+
+export function render(
+	pixelX: number,
+	pixelY: number,
+	spheredataX: number,
+	spheredataY: number,
+	spheredataZ: number,
+	spheredataScale: number
+): u32 {
+	const spheres = [
+		new Sphere(new Vector(0, 4.5, 0), 4, new Vector(128, 0, 255)),
+		new Sphere(
+			new Vector(spheredataX, spheredataY, spheredataZ),
+			spheredataScale,
+			new Vector(255, 128, 0)
+		),
+	];
+	let bounces: number = 2;
 	const u = pixelX;
 	const v = pixelY;
 
-	//let R = u32(u * 255);
-	//let G = u32(v * 255);
-	//let B = u32(0);
-	//return u32(0x00000000 | (R << 16) | (G << 8) | B);
-
-	let lightDirection = new Vector(-2, -3, 2).normalize();
-
+	let lightDirection = new Vector(-1, -1, 1).normalize();
 	let rayOrigin = new Vector(0, 0, 1).normalize();
 	let rayDirection = new Vector(u, v, -1).normalize();
-	let t = sphere.intersect(new Ray(rayOrigin, rayDirection));
+	let multiplier: f64 = 1;
 
-	if (t == -1) {
-		return 0x00000000;
-	} else {
-		let hitPos = rayOrigin.add(rayDirection.multiply(t));
+	let RGBValues = [0, 0, 0];
 
-		let normal = hitPos.sub(sphere.center).normalize();
-		let light = Math.max(0, normal.dot(lightDirection)); // Calculate the light intensity at the hit position
+	for (let i = 0; i < bounces; i++) {
+		let hitInfo = traceRay(rayOrigin, rayDirection, spheres);
 
-		//let R = u32((normal.x * 0.5 + 0.5) * 255);
-		//let G = u32((normal.y * 0.5 + 0.5) * 255);
-		//let B = u32((normal.z * 0.5 + 0.5) * 255);
+		if (hitInfo.didHit) {
+			let RGBVal = calculateColor(
+				hitInfo,
+				lightDirection,
+				spheres,
+				<i32>hitInfo.Index
+			);
 
-		let R = u32(Math.min(Math.max(0, 204 * light), 255));
-		let G = u32(Math.min(Math.max(0, 153 * light), 255));
-		let B = u32(Math.min(Math.max(0, 255 * light), 255));
+			for (let j = 0; j < RGBValues.length; j++) {
+				RGBValues[j] += <i32>Math.round(<f64>RGBVal[j] * multiplier);
+			}
 
-		return 0x00000000 | (R << 16) | (G << 8) | B;
+			multiplier *= <f64>0.7;
+
+			rayOrigin = hitInfo.Position.add(hitInfo.Normal.multiply(0.0001));
+			rayDirection = rayDirection.reflect(hitInfo.Normal);
+		}
 	}
+
+	return 0x00000000 | (RGBValues[0] << 16) | (RGBValues[1] << 8) | RGBValues[2];
 }
 
 /*
 			//normals
-			let r = u32((normal.x * 0.5 + 0.5) * 255);
-			let g = u32((normal.y * 0.5 + 0.5) * 255);
-			let b = u32((normal.z * 0.5 + 0.5) * 255);
+			let R = u32((normal.x * 0.5 + 0.5) * 255);
+			let G = u32((normal.y * 0.5 + 0.5) * 255);
+			let B = u32((normal.z * 0.5 + 0.5) * 255);
+
+
+			//UV
+			let R = u32(u * 255);
+			let G = u32(v * 255);
+			let B = u32(0);
+			return u32(0x00000000 | (R << 16) | (G << 8) | B);
 	*/
--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
 	const [wasmModule, setWasmModule] = useState(null);
 	const [imageData, setImageData] = useState(null);
 
+	const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
 	useEffect(() => {
 		fetch("/release.wasm")
 			.then((response) => response.arrayBuffer())
@@ -52,63 +54,90 @@ function App() {
 		return [red, green, blue];
 	}
 
+	async function RenderFrame() {
+		// Get a reference to the canvas and its context
+		const canvas = document.getElementById("canvas");
+		const ctx = canvas.getContext("2d");
+
+		ctx.imageSmoothingEnabled = false;
+
+		// Set the canvas dimensions
+		const width = canvas.width;
+		const height = canvas.height;
+
+		ctx.fillStyle = "rgb(0, 0, 0, 255)";
+		ctx.fillRect(0, 0, width, height);
+		console.log("cleared");
+
+		// Create an ImageData object
+		const imgData = ctx.createImageData(width, height);
+
+		let X = document.getElementById("X").value;
+		let Y = document.getElementById("Y").value;
+		let Z = document.getElementById("Z").value;
+		let Scale = document.getElementById("Scale").value;
+
+		for (let y = 0; y < height; y++) {
+			for (let x = 0; x < width; x++) {
+				// Call the render function to get the hit info for the current pixel
+
+				const pointer = wasmModule.exports.render(
+					(Number.parseFloat(x) / width) * 2 - 1,
+					(Number.parseFloat(y) / height) * 2 - 1,
+					X,
+					Y,
+					Z,
+					Scale
+				);
+
+				const memoryBuffer = new Uint8Array(wasmModule.exports.memory.buffer);
+				const dataView = new DataView(memoryBuffer.buffer);
+
+				let color = largeIntToRGB(pointer);
+
+				//const r = dataView.getFloat32(pointer);
+				//const g = dataView.getFloat32(pointer + 4); // Next 4 bytes
+				//const b = dataView.getFloat32(pointer + 8); // Next 4 bytes after g
+
+				//console.log(r + " " + g + " " + b);
+				const index = (y * width + x) * 4;
+				//console.log(r);
+				imgData.data[index + 0] = color[0]; // Red
+				imgData.data[index + 1] = color[1]; // Green
+				imgData.data[index + 2] = color[2]; // Blue
+				imgData.data[index + 3] = 255; // Alpha (255 is fully opaque)
+			}
+		}
+
+		// Put the image data on the canvas
+		ctx.putImageData(imgData, 0, 0);
+		await delay(1000);
+
+		console.log(document.getElementById("CB").value);
+		if (document.getElementById("CB").value == "on") {
+			//RenderFrame();
+		}
+	}
+
 	return (
 		<div className="App">
 			<canvas width={800} height={800} id="canvas" />
 			<button
-				onClick={() => {
-					// Get a reference to the canvas and its context
-					const canvas = document.getElementById("canvas");
-					const ctx = canvas.getContext("2d");
-
-					ctx.imageSmoothingEnabled = false;
-
-					// Set the canvas dimensions
-					const width = canvas.width;
-					const height = canvas.height;
-					ctx.fillStyle = "rgb(0, 0, 0, 255)";
-					ctx.fillRect(0, 0, width, height);
-					// Create an ImageData object
-					const imgData = ctx.createImageData(width, height);
-
-					for (let y = 0; y < height; y++) {
-						for (let x = 0; x < width; x++) {
-							// Call the render function to get the hit info for the current pixel
-
-							const pointer = wasmModule.exports.render(
-								(Number.parseFloat(x) / width) * 2 - 1,
-								(Number.parseFloat(y) / height) * 2 - 1,
-								width,
-								height
-							);
-
-							const memoryBuffer = new Uint8Array(
-								wasmModule.exports.memory.buffer
-							);
-							const dataView = new DataView(memoryBuffer.buffer);
-
-							let color = largeIntToRGB(pointer);
-
-							//const r = dataView.getFloat32(pointer);
-							//const g = dataView.getFloat32(pointer + 4); // Next 4 bytes
-							//const b = dataView.getFloat32(pointer + 8); // Next 4 bytes after g
-
-							//console.log(r + " " + g + " " + b);
-							const index = (y * width + x) * 4;
-							//console.log(r);
-							imgData.data[index + 0] = color[0]; // Red
-							imgData.data[index + 1] = color[1]; // Green
-							imgData.data[index + 2] = color[2]; // Blue
-							imgData.data[index + 3] = 255; // Alpha (255 is fully opaque)
-						}
-					}
-
-					// Put the image data on the canvas
-					ctx.putImageData(imgData, 0, 0);
+				onClick={async () => {
+					RenderFrame();
 				}}
 			>
 				BUTTON
 			</button>
+			<label>X: </label>
+			<input id="X" step={0.1} type="number"></input>
+			<label>Y: </label>
+			<input id="Y" step={0.1} type="number"></input>
+			<label>Z: </label>
+			<input id="Z" step={0.1} type="number"></input>
+			<label>Scale: </label>
+			<input id="Scale" step={0.1} type="number"></input>
+			<input id="CB" type="checkbox"></input>
 		</div>
 	);
 }
